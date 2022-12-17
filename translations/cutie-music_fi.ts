7a6a0df7d15826a5428d24bb35a1cabc8279d73f
Erik Inkinen <erik.inkinen@gmail.com>
2022-12-17 21:54:32 +0200

    [translations] Translate into FInnish

---
+++ translations/cutie-music_fi.ts
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi_FI">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../src/qml/main.qml" line="27"/>
+        <location filename="../src/qml/main.qml" line="165"/>
+        <source>Music</source>
+        <translation>Musiikki</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/main.qml" line="132"/>
+        <source>%1 - %2</source>
+        <translation>%1 - %2</translation>
+    </message>
+</context>
+</TS>
